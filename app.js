c593462303d2bc5f2d9a0ca1dcdf1b6df3d83e4d
Vishrutha V B <Vishruthavb24@gmail.com>
2025-04-30 12:50:53 +0530

    Login and logout

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
-const Listing = require("../MajorProject/models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("../MajorProject/models/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 
@@ -31,61 +37,45 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        expires:Date.now() + 7 * 24 * 60 *1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
 app.get("/",(req,res)=>{
     res.send("Hi I am root");
 });
 
-const validateListing = (req, res, next) =>{
-  let {error} = listingSchema.validate(req.body);
 
-  if(error){
-    let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else{
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+    res.locals.success = req.flash("success");
+    // console.log(res.locals.success);
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
     next();
-  }
-};
+});
 
-const validateReview = (req, res, next) =>{
-  let {error} = reviewSchema.validate(req.body);
-
-  if(error){
-    let errMsg = error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else{
-    next();
-  }
-};
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter); 
+app.use("/",userRouter);
 
 
-
-
-
-  //Reviews
-  //POST Route
-  app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) =>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    // console.log("New review saved");
-    // res.send("new review saved");
-    res.redirect(`/listings/${listing._id}`);
-  }));
-
-
-  app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let{id,reviewId} = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-  }))
   
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing = new Listing({
@@ -103,16 +93,14 @@ const validateReview = (req, res, next) =>{
 
 app.all("*", (req, res, next)=>{
   next(new ExpressError(404,"Page Not Found"))
-})
-
-
+});
 
 app.use((err,req,res,next)=>{
   let {statusCode = 500, message="Something went wrong"} = err;
   res.status(statusCode).render("error.ejs",{ message });
   // res.status(statusCode).render("error.ejs",{ err });
   // res.status(statusCode).send(message);
-})
+});
 
 
 app.listen(8080,() =>{
